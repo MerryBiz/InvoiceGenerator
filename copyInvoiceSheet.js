06ef27a20254d67f258b28df0c7a549c019d7900
hirokazu1029 <noa306@gmail.com>
2023-08-26 09:56:28 +0900

    各種情報のバリデーション ＋ フォーマット

--- copyInvoiceSheet.js
+++ copyInvoiceSheet.js
@@ -1,22 +1,23 @@
 function copyInvoiceSheet() {
-  
+
   const attendanceSheet = SpreadsheetApp.getActiveSheet();  // 今月の勤務実績表シート
   const spreadsheet = attendanceSheet.getParent();
   const invoiceInfoSheet = spreadsheet.getSheetByName("各種情報");
-const basicInfoSheet = spreadsheet.getSheetByName("基本情報");
-const invoiceTemplateSheet = spreadsheet.getSheetByName("請求書テンプレート");
-const attendanceSheetName = attendanceSheet.getName();  // 今月の勤務実績表のシート名
-const invoiceSheetName = attendanceSheetName + "_請求書"; // 今月の請求書シートのシート名
+  const basicInfoSheet = spreadsheet.getSheetByName("基本情報");
+  const invoiceTemplateSheet = spreadsheet.getSheetByName("請求書テンプレート");
+  const attendanceSheetName = attendanceSheet.getName();  // 今月の勤務実績表のシート名
+  const invoiceSheetName = attendanceSheetName + "_請求書"; // 今月の請求書シートのシート名
 
 
-const invoiceBodyRng = "B6:B9"; //請求書の内容の範囲
- 
+  const invoiceBodyRng = "B6:B9"; //請求書の内容の範囲
+
   if (!invoiceTemplateSheet) {
     throw new Error("請求書テンプレートシートが見つかりませんでした。");
   }
 
   // 同名のシートが存在する場合はエラーを出力する
   if (spreadsheet.getSheetByName(attendanceSheetName + "_請求書")) {
+    SpreadsheetApp.getUi().alert('エラー', '既に該当月の請求書が作成されています', SpreadsheetApp.getUi().ButtonSet.OK);
     throw new Error("同名のシートがすでに存在しています。");
   }
   var invoiceSheet = invoiceTemplateSheet.copyTo(spreadsheet);
@@ -36,6 +37,12 @@ const invoiceBodyRng = "B6:B9"; //請求書の内容の範囲
   const staffAddress1 = invoiceInfoSheet.getRange("C8").getValue();
   const staffAddress2 = invoiceInfoSheet.getRange("C9").getValue();
 
+  //基本情報バリデーション
+  if (!accountName || !staffID || !accountDescription || !companyName || !staffName || !zipCode || !staffAddress1 || !staffAddress2) {
+    SpreadsheetApp.getUi().alert('エラー', '各種情報の必須項目に空欄があります。各種情報シートの更新を行ってください', SpreadsheetApp.getUi().ButtonSet.OK);
+    throw new Error("各種情報シートの情報取得エラー");
+  }
+
   var year = new Date().getFullYear();
   var month = new Date().getMonth() + 1;
   if (month === 13) {
@@ -44,12 +51,12 @@ const invoiceBodyRng = "B6:B9"; //請求書の内容の範囲
   }
   var lastDay = new Date(year, month, 0);
 
-  var invoiceDay = new Date(year, month-1, 0);
+  var invoiceDay = new Date(year, month - 1, 0);
 
-  const valuesToSetAccountInfo = [    [attendanceSheetName + "リモートスタッフ稼働分"],
-    [lastDay],
-    [accountDescription],
-    [accountName],
+  const valuesToSetAccountInfo = [[attendanceSheetName + "リモートスタッフ稼働分"],
+  [lastDay],
+  [accountDescription],
+  [accountName],
   ];
 
   invoiceSheet.getRange(invoiceBodyRng).setValues(valuesToSetAccountInfo);
@@ -71,7 +78,7 @@ const invoiceBodyRng = "B6:B9"; //請求書の内容の範囲
 
   const dataToSet = [];
   const rowsToCopy = [];
-  
+
   for (let row = 6; row < data.length; row++) {
     if (!data[row][2] && !data[row][3] && !data[row][4] && !data[row][6] && !data[row][7]) {
       break;
@@ -82,13 +89,13 @@ const invoiceBodyRng = "B6:B9"; //請求書の内容の範囲
 
   for (let i = 0; i < rowsToCopy.length; i++) {
     const row = rowsToCopy[i];
-    const rowData = [      data[row][2],
+    const rowData = [data[row][2],
       "",
       "",
-      data[row][3],
-      data[row][4],
-      data[row][6],
-      data[row][7],
+    data[row][3],
+    data[row][4],
+    data[row][6],
+    data[row][7],
     ];
     dataToSet.push(rowData);
   }
@@ -99,15 +106,15 @@ const invoiceBodyRng = "B6:B9"; //請求書の内容の範囲
   invoiceSheet.getRange(startRow, 1, numRowsToSet, numColsToSet).setValues(dataToSet);
   //最終rangeに書き込みがされていない場合、無限ループ
   var rangechk = invoiceSheet.getRange("A15").getValue();
-  while (rangechk == ""){
+  while (rangechk == "") {
     rangechk = invoiceSheet.getRange("A15").getValue();
   }
-  
+
   // invoiceNoが空欄の場合、A28からD32を削除
   if (invoiceNo == "") {
-  invoiceSheet.getRange("A28:D32").clear();
-  invoiceSheet.getRange("H29").setValue(0);
-  Utilities.sleep(1000);
+    invoiceSheet.getRange("A28:D32").clear();
+    invoiceSheet.getRange("H29").setValue(0);
+    Utilities.sleep(1000);
   }
 }
 
